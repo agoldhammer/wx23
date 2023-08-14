c1480d846a916aebf0aa7cf2f5f4390781e633f2
Art Goldhammer <art.goldhammer@gmail.com>
2023-08-14 16:37:16 -0400

    serving geo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  export let data;
+  //   console.log(data);
+</script>
+
+<p>{data.name}</p>
+<p>{data.country}</p>
+<p>{data.lat}, {data.lon}</p>
