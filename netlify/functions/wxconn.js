b5316e3bfa2fa52fe32bc659d6ed005d884a11db
Arthur Goldhammer <art.goldhammer@gmail.com>
2023-08-14 20:20:33 -0400

    parametrize geo search

--- netlify/functions/wxconn.js
+++ netlify/functions/wxconn.js
@@ -1,11 +1,12 @@
 const appid = process.env.appid;
 
 exports.handler = async event => {
-  let country = event.queryStringParameters.country;
   let city = event.queryStringParameters.city;
+  let country = event.queryStringParameters.country;
+  console.log ('citycoun', city, country);
 
   let response = await fetch (
-    `http://api.openweathermap.org/geo/1.0/direct?q=Berlin%2CDE&limit=1&appid=${appid}`,
+    `http://api.openweathermap.org/geo/1.0/direct?q=${city}%2C${country}&limit=1&appid=${appid}`,
     {
       method: 'GET',
     }
@@ -14,9 +15,9 @@ exports.handler = async event => {
   let geodata_list = await response.json ();
   let geodata = geodata_list[0];
   // let {name, country, lat, lon} = geodata;
-  console.log ('wxconn', geodata);
+  // console.log ('wxconn', geodata);
   let retval = JSON.stringify ({geodata: geodata});
-  console.log ('retval', retval);
+  // console.log ('retval', retval);
   return {
     statusCode: 200,
     body: retval,
