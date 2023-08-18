9b796639d54a4ccace64d3368c09cee346284e41
Arthur Goldhammer <art.goldhammer@gmail.com>
2023-08-17 20:23:39 -0400

    mucking with type of dataLine

--- src/lib/Wx.svelte
+++ src/lib/Wx.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
   import { Line } from "svelte-chartjs";
   import { Chart } from "chart.js/auto";
+  import type { ChartData, ChartDataset } from "chart.js";
+  // type CD = typeof ChartData;
   export let wxdata: any;
 
-  // $: keys = wxdata.keys();
-
-  let dataLine = {
-    // labels: ["January", "February", "March", "April", "May", "June", "July"],
+  // @ts-ignore: Object is possibly 'null'.
+  let dataLine: ChartData<"line"> = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
     labels: wxdata.times_text,
     // labels: wxdata.times,
     datasets: [
@@ -31,29 +32,29 @@
         pointHitRadius: 10,
         // data: [65, 59, 80, 81, 56, 55, 40],
         data: wxdata.temps,
-      },
-      {
-        label: "mintemps",
-        fill: true,
-        lineTension: 0.3,
-        backgroundColor: "rgba(184, 185, 210, .3)",
-        borderColor: "rgb(35, 26, 136)",
-        borderCapStyle: "butt",
-        borderDash: [],
-        borderDashOffset: 0.0,
-        borderJoinStyle: "miter",
-        pointBorderColor: "rgb(35, 26, 136)",
-        pointBackgroundColor: "rgb(255, 255, 255)",
-        pointBorderWidth: 10,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: "rgb(0, 0, 0)",
-        pointHoverBorderColor: "rgba(220, 220, 220, 1)",
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 10,
-        // data: [28, 48, 40, 19, 86, 27, 90],
-        data: wxdata.mintemps,
-      },
+      } as ChartDataset<"line">,
+      // {
+      //   label: "mintemps",
+      //   fill: true,
+      //   lineTension: 0.3,
+      //   backgroundColor: "rgba(184, 185, 210, .3)",
+      //   borderColor: "rgb(35, 26, 136)",
+      //   borderCapStyle: "butt",
+      //   borderDash: [],
+      //   borderDashOffset: 0.0,
+      //   borderJoinStyle: "miter",
+      //   pointBorderColor: "rgb(35, 26, 136)",
+      //   pointBackgroundColor: "rgb(255, 255, 255)",
+      //   pointBorderWidth: 10,
+      //   pointHoverRadius: 5,
+      //   pointHoverBackgroundColor: "rgb(0, 0, 0)",
+      //   pointHoverBorderColor: "rgba(220, 220, 220, 1)",
+      //   pointHoverBorderWidth: 2,
+      //   pointRadius: 1,
+      //   pointHitRadius: 10,
+      //   // data: [28, 48, 40, 19, 86, 27, 90],
+      //   data: wxdata.mintemps,
+      // } as ChartDataset<"line">,
     ],
   };
 </script>
