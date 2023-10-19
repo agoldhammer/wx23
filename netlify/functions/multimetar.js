fc29011b4629795769b9ad89288bda1a88b57b26
Arthur Goldhammer <art.goldhammer@gmail.com>
2023-10-19 11:01:00 -0400

    added multimetar.js

---
+++ netlify/functions/multimetar.js
@@ -0,0 +1,14 @@
+exports.handler = async event => {
+  const icaos = ['KBOS', 'KBED', 'KOWD', 'KORH', 'KMHT'];
+  const encoded_icaos = encodeURIComponent (icaos.join ());
+  const url = `https://aviationweather.gov/api/data/metar/?ids=${encoded_icaos}`;
+  const response = await fetch (url);
+  const text = await response.text ();
+  // must slice off last item in split, which will be blank b/c of trailing newline
+  const metars = text.split ('\n').slice (0, -1);
+  console.log ('metar list', metars);
+  return {
+    statusCode: 200,
+    body: JSON.stringify (metars),
+  };
+};
