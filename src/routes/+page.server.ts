86483a5fdccb2bd35935127056912cef0cd45ed7
Arthur Goldhammer <art.goldhammer@gmail.com>
2023-10-21 06:00:31 -0400

    local load is working

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,40 @@
+import { error } from "@sveltejs/kit";
+import { appid } from "$env/static/private";
+
+export async function load({ params }) {
+  // metars part
+  const icaos = ["KBOS", "KBED", "KOWD", "KORH", "KMHT"];
+  const encoded_icaos = encodeURIComponent(icaos.join());
+  const url = `https://aviationweather.gov/api/data/metar/?ids=${encoded_icaos}`;
+  const response = await fetch(url);
+  const text = await response.text();
+  // must slice off last item in split, which will be blank b/c of trailing newline
+  const metars = text.split("\n").slice(0, -1);
+
+  // local weather part
+  let city = "Cambridge";
+  let country = "US";
+  //   let city = event.queryStringParameters.city;
+  //   let country = event.queryStringParameters.country;
+  // console.log ('local', city, country);
+
+  let geoapi = `http://api.openweathermap.org/geo/1.0/direct?q=${city}%2C${country}&limit=1&appid=${appid}`;
+  let response2 = await fetch(geoapi, { method: "GET" });
+
+  let geodata_list = await response2.json();
+  let geodata = geodata_list[0];
+  let { lat, lon } = geodata;
+  // console.log ('local1', lat, lon);
+
+  let wxapi = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appid}&units=imperial`;
+
+  let local_wx = await fetch(wxapi, { method: "GET" });
+  let retval = await local_wx.json();
+  // console.log ('local2', retval);
+  let body = { metars: metars, local_wx_data: retval };
+  //   console.log("body in server", body);
+  return {
+    statusCode: 200,
+    body: JSON.stringify(body),
+  };
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,14 @@
   // export let data;
   // console.log("page", data);
   import App from "$lib/App.svelte";
+  import Metar from "$lib/Metar.svelte";
+  export let data;
+  console.log(data);
+  const body = JSON.parse(data.body);
+  console.log(body.metars);
+  let metars = body.metars;
+  const local_wx_data = body.local_wx_data;
+  console.log(local_wx_data);
 </script>
 
 <svelte:head>
@@ -11,4 +19,70 @@
   <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
   <link rel="manifest" href="site.webmanifest" />
 </svelte:head>
-<App />
+
+<!-- <App /> -->
+<!-- <Metar > -->
+<div id="locdata" class="locwxctr">
+  <div class="wx">
+    <h3>Cambridge, MA</h3>
+    <span>Temp: {local_wx_data.main.temp}</span>
+    <span>Feels like: {local_wx_data.main.feels_like}</span>
+    <span
+      >Temp range: {local_wx_data.main.temp_min}-{local_wx_data.main
+        .temp_max}</span
+    >
+    <span>Pressure: {local_wx_data.main.pressure}</span>
+    <span>Humidity: {local_wx_data.main.humidity}</span>
+    <span>Wind: {local_wx_data.wind.deg}° @ {local_wx_data.wind.speed} mph</span
+    >
+    {#if local_wx_data.wind.gust}
+      <span>Gust: {local_wx_data.wind.gust}</span>
+    {/if}
+    <span>Visibility: {local_wx_data.visibility}</span>
+    <span>Weather: {local_wx_data.weather[0].description}</span>
+  </div>
+  <div class="metar">
+    <h3>METARS</h3>
+    {#each metars as metar}
+      <Metar {metar} />
+    {/each}
+  </div>
+</div>
+
+<!-- </div> -->
+
+<style>
+  .locwxctr {
+    display: grid;
+    height: 100%;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: repeat(3, 1fr);
+    grid-template-areas:
+      "wx metar"
+      "wx metar"
+      "wx metar";
+    column-gap: 3em;
+    background-image: linear-gradient(to right, sienna, goldenrod);
+    border: 4px solid darkgray;
+    border-radius: 10px;
+    color: white;
+    /* text-align: left; */
+    margin-right: 20px;
+    padding: 20px;
+  }
+
+  .wx {
+    grid-area: wx;
+    padding: 5px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+  }
+
+  .metar {
+    grid-area: metar;
+    display: flex;
+    flex-direction: column;
+    overflow-y: auto;
+  }
+</style>
