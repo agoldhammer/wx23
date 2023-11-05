4841852e97610ca9301e4b95e4ac472762a5934c
Art Goldhammer <art.goldhammer@gmail.com>
2023-11-05 13:07:21 -0500

    added err hdlr for city data; cleaned up old debug

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -14,9 +14,6 @@ export async function load({ params }) {
   // local weather part
   let city = "Cambridge";
   let country = "US";
-  //   let city = event.queryStringParameters.city;
-  //   let country = event.queryStringParameters.country;
-  // console.log ('local', city, country);
   try {
     let geoapi = `http://api.openweathermap.org/geo/1.0/direct?q=${city}%2C${country}&limit=1&appid=${appid}`;
     let response2 = await fetch(geoapi, { method: "GET" });
@@ -24,15 +21,12 @@ export async function load({ params }) {
     let geodata_list = await response2.json();
     let geodata = geodata_list[0];
     let { lat, lon } = geodata;
-    // console.log ('local1', lat, lon);
 
     let wxapi = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appid}&units=imperial`;
 
     let local_wx = await fetch(wxapi, { method: "GET" });
     let retval = await local_wx.json();
-    // console.log ('local2', retval);
     let body = { metars: metars, local_wx_data: retval };
-    //   console.log("body in server", body);
     return {
       statusCode: 200,
       body: JSON.stringify(body),
--- src/routes/[group]/+page.server.ts
+++ src/routes/[group]/+page.server.ts
@@ -1,6 +1,6 @@
 // import { error } from "@sveltejs/kit";
 import { appid } from "$env/static/private";
-// import type { City } from "$lib/Cities";
+import { error } from "@sveltejs/kit";
 import { group_to_city_list } from "$lib/Cities";
 
 export async function load({ params }) {
@@ -8,43 +8,47 @@ export async function load({ params }) {
   /* ! Fetch forecasts and current weather for each citystate in group */
   // console.log("server", group);
   const city_list = group_to_city_list(group);
-  const geodata_raw = city_list.map((item) =>
-    fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${item.city}%2C${item.country}&limit=1&appid=${appid}`
-    ).then((resp) => resp.json().then((item) => item[0]))
-  );
-  const geodata = await Promise.all(geodata_raw);
-  const coords = geodata.map((item) => [item.lat, item.lon]);
-  //   console.log(coords);
-  /* ! for each pair of city coords, fetch weather data */
-  //
-  const wxdata_raw: any[] = coords.map((coord) =>
-    fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?lat=${coord[0]}&lon=${coord[1]}&appid=${appid}&units=imperial`
-    ).then((resp) => resp.json())
-  );
 
-  const wxdata_group = await Promise.all(wxdata_raw);
+  try {
+    const geodata_raw = city_list.map((item) =>
+      fetch(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${item.city}%2C${item.country}&limit=1&appid=${appid}`
+      ).then((resp) => resp.json().then((item) => item[0]))
+    );
+    const geodata = await Promise.all(geodata_raw);
+    const coords = geodata.map((item) => [item.lat, item.lon]);
+    //   console.log(coords);
+    /* ! for each pair of city coords, fetch weather data */
+    //
+    const wxdata_raw: any[] = coords.map((coord) =>
+      fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${coord[0]}&lon=${coord[1]}&appid=${appid}&units=imperial`
+      ).then((resp) => resp.json())
+    );
 
-  /* ! need current weather for each city */
-  let current_wxs_promises = coords.map((coord) =>
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${coord[0]}&lon=${coord[1]}&appid=${appid}&units=imperial`,
-      { method: "GET" }
-    ).then((resp) => resp.json())
-  );
-  const current_wxs = await Promise.all(current_wxs_promises);
-  // console.log("currents", current_wxs);
+    const wxdata_group = await Promise.all(wxdata_raw);
 
-  for (let i = 0; i < city_list.length; i++) {
-    wxdata_group[i].citystate = city_list[i];
-    wxdata_group[i].current = current_wxs[i];
+    /* ! need current weather for each city */
+    let current_wxs_promises = coords.map((coord) =>
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${coord[0]}&lon=${coord[1]}&appid=${appid}&units=imperial`,
+        { method: "GET" }
+      ).then((resp) => resp.json())
+    );
+    const current_wxs = await Promise.all(current_wxs_promises);
+    for (let i = 0; i < city_list.length; i++) {
+      wxdata_group[i].citystate = city_list[i];
+      wxdata_group[i].current = current_wxs[i];
+    }
+
+    // console.log("svr augmented", wxdata_group);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ wxdata_group: wxdata_group }),
+    };
+  } catch (err: any) {
+    console.error("Error fetching city data", err);
+    throw error(404, `While fetching city data: ${err.cause}`);
   }
-
-  // console.log("svr augmented", wxdata_group);
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ wxdata_group: wxdata_group }),
-  };
 }
