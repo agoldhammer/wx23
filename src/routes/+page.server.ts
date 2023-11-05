41ba8402a4984f2b93e82d384100301a72159e5c
Art Goldhammer <art.goldhammer@gmail.com>
2023-11-05 12:56:01 -0500

    added err hdlr for home data fetch

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -17,24 +17,28 @@ export async function load({ params }) {
   //   let city = event.queryStringParameters.city;
   //   let country = event.queryStringParameters.country;
   // console.log ('local', city, country);
+  try {
+    let geoapi = `http://api.openweathermap.org/geo/1.0/direct?q=${city}%2C${country}&limit=1&appid=${appid}`;
+    let response2 = await fetch(geoapi, { method: "GET" });
 
-  let geoapi = `http://api.openweathermap.org/geo/1.0/direct?q=${city}%2C${country}&limit=1&appid=${appid}`;
-  let response2 = await fetch(geoapi, { method: "GET" });
+    let geodata_list = await response2.json();
+    let geodata = geodata_list[0];
+    let { lat, lon } = geodata;
+    // console.log ('local1', lat, lon);
 
-  let geodata_list = await response2.json();
-  let geodata = geodata_list[0];
-  let { lat, lon } = geodata;
-  // console.log ('local1', lat, lon);
+    let wxapi = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appid}&units=imperial`;
 
-  let wxapi = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appid}&units=imperial`;
-
-  let local_wx = await fetch(wxapi, { method: "GET" });
-  let retval = await local_wx.json();
-  // console.log ('local2', retval);
-  let body = { metars: metars, local_wx_data: retval };
-  //   console.log("body in server", body);
-  return {
-    statusCode: 200,
-    body: JSON.stringify(body),
-  };
+    let local_wx = await fetch(wxapi, { method: "GET" });
+    let retval = await local_wx.json();
+    // console.log ('local2', retval);
+    let body = { metars: metars, local_wx_data: retval };
+    //   console.log("body in server", body);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(body),
+    };
+  } catch (err: any) {
+    console.error("Error fetching home page", err);
+    throw error(404, `While fetching data for home location: ${err.cause}`);
+  }
 }
